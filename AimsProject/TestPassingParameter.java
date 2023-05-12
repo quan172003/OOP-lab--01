ba1e973a77a0c948642ce51eda304b649d0a5520
quan20215236 <hoangminhquan172003@gmail.com>
2023-05-12 09:02:42 +0700

    ubdate

---
+++ AimsProject/TestPassingParameter.java
@@ -0,0 +1,9 @@
+package AimsProject;
+
+public class TestPassingParameter {
+    public static void main(String args[])
+    {
+        DigitalVideoDics jungleDVD = new DigitalVideoDics("Jungle");
+
+    }
+}
